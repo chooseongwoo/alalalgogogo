7e51e9b2f4936811e108422299a5c6492f05c7de
ChooSeongWoo <sldoal2423@gmail.com>
2023-07-22 22:22:04 +0900

    1169

---
+++ codeup/1169.py
@@ -0,0 +1,10 @@
+a = int(input())
+
+birth_year = 2012 - a + 1
+
+if(birth_year < 2000):
+    birth_year = str(birth_year)
+    print(int(birth_year[2:4]),1)
+else:
+    birth_year = str(birth_year)
+    print(int(birth_year[2:4]),3)
