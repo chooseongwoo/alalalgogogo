d73eb8becae9ff4514068ce5b0265b607510d08c
ChooSeongWoo <sldoal2423@gmail.com>
2023-07-30 23:00:11 +0900

    진수 변환

--- chap02/card_conv.py
+++ chap02/card_conv.py
@@ -11,3 +11,25 @@ def card_conv(x: int, r: int) -> str:
         x //= r
 
     return d[::-1] # 역순으로 반환
+
+
+
+if __name__ == '__main__':
+    print('10진수를 n진수로 변환합니다.')
+
+    while True:
+        while True: # 음이 아닌 정수를 입력받음
+            no = int(input('변환할 값으로 음이 아닌 정수를 입력하세요. : '))
+            if no > 0:
+                break
+        while True: # 2~36진수의 정숫값을 입력받음
+            cd = int(input('어떤 진수로 변환할까요? : '))
+            if 2 <= cd <= 36:
+                break
+        
+        print(f'{cd}진수로는 {card_conv(no, cd)}입니다.')
+
+        retry = input('한 번 더 변환할까요?(Y ... 예 / N ... 아니요) :')
+
+        if retry in {'N','n'}:
+            break
