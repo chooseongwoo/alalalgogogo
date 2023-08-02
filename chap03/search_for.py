9241ced42d08d333b666162871dc142868a6a5e5
ChooSeongWoo <sldoal2423@gmail.com>
2023-08-02 22:59:29 +0900

    search_for

--- chap03/search_for.py
+++ chap03/search_for.py
@@ -1,18 +1,13 @@
-# while 문으로 작성한 선형 검색 알고리즘
+# for 문으로 작성한 선형 검색 알고리즘
 
 from typing import Any, Sequence
 
-def seq_search(a:Sequence, key:any) -> int:
-    """시퀀스 a에서 key와 값이 같은 원소를 선형 검색(while 문)"""
-    i = 0
-
-    while True:
-        if i == len(a):
-            return -1 # 검색에 실패하여 -1을 반환
+def seq_search(a: Sequence, key: Any) -> int:
+    """시퀀스 a에서 key와 값이 같은 원소를 선형 검색(for 문)"""
+    for i in range(len(a)):
         if a[i] == key:
-            return i # 검색에 성공하여 현재 검사한 배열의 인덱스를 반환
-        i += 1
-
+            return i # 검색 성공(인덱스를 반환)
+    return -1 # 검색 실패(-1을 반환)
 if __name__ == '__main__':
     num = int(input('원소 수를 입력하세요. : ')) # num값을 입력받음
     x = [None] * num # 원소 수가 num인 배열을 생성
