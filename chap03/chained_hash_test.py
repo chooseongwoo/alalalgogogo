ffac12aa4fb52842730b296a208bd245fb02b471
ChooSeongWoo <sldoal2423@gmail.com>
2023-08-07 19:32:34 +0900

    chained_hash_test

---
+++ chap03/chained_hash_test.py
@@ -0,0 +1,42 @@
+from enum import Enum
+from chained_hash import ChainedHash
+
+Menu = Enum('Menu', ['추가', '삭제', '검색', '덤프', '종료'])
+
+def select_menu() -> Menu:
+    """메뉴 선택"""
+    s = [f'({m.value}){m.name}' for m in Menu]
+    while True:
+        print(*s, sep ='    ', end='')
+        n = int(input(': '))
+        if 1 <= n <= len(Menu):
+            return Menu(n)
+
+hash = ChainedHash(13)
+
+while True:
+    menu = select_menu()  # 등호(==)를 할당 연산자(=)로 수정
+
+    if menu == Menu.추가:
+        key = int(input('추가할 키를 입력하세요. : '))
+        val = input('추가할 값을 입력하세요. : ')
+        if not hash.add(key, val):
+            print('추가를 실패했습니다!')
+
+    elif menu == Menu.삭제:
+        key = int(input('삭제할 키를 입력하세요. : '))  # 입력 프롬프트 수정
+        if not hash.remove(key):  # remove 함수로 수정
+            print('삭제를 실패했습니다!')
+
+    elif menu == Menu.검색:
+        key = int(input('검색할 키를 입력하세요. : '))
+        t = hash.search(key)
+        if t is not None:
+            print(f'검색한 값은 {t}입니다.')  # 출력 메시지 수정
+        else:
+            print('검색한 데이터가 없습니다.')
+
+    elif menu == Menu.덤프:
+        hash.dump()
+    else:
+        break
